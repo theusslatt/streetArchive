9342f307b051866ef87fd61d18f67139462e95d2
Matheus Silveira <matheus@exemplo.com>
2023-08-27 21:45:47 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.menu-hamburguer').click(function(){
+        $('nav').slideToggle()
+    })
+
+    $('button').click(function(){
+        alert('"Bentley Truck" foi adicionado ao seu carrinho')
+    })
+})
